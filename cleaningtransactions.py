5e5893aa53362fe394ae62341c47346edf0d8afd
Alvaro-bit-stack <aizquierdo1325@gmail.com>
2025-06-21 13:30:15 -0400

    Updated how the table transactions is made, changed connection to supabase

--- cleaningtransactions.py
+++ cleaningtransactions.py
@@ -1,18 +1,23 @@
 import pandas as pd
 import psycopg2
 import os
-
+from dotenv import load_dotenv
+import psycopg2.extras
+# Load environment variables from .env file
+load_dotenv()
 #CONFIG
-CSV_PATH = os.path.abspath("C:/Users/alvar/OneDrive/Desktop/Personal Finance/data/transactions.csv")
+CSV_PATH = os.path.abspath("C:/Users/alvar/OneDrive/Desktop/Personal_FInance_deploy/data/transactions.csv")
+
 
 
 DB_CONFIG = {
-    "host": "localhost",
-    "database": "personal_finance",
-    "user": "postgres",
-    "password": "Alvaro1325@",
-    "port": 5432
+    "host": os.getenv("host"),
+    "database": os.getenv("dbname"),
+    "user": os.getenv("user"),
+    "password": os.getenv("password"),
+    "port": os.getenv("port")
 }
+
 #Connecting to the database
 conn = psycopg2.connect(**DB_CONFIG)
 cursor = conn.cursor()
@@ -34,20 +39,30 @@ df = df.drop(["Debit","Credit"],axis=1)
 
 
 #Cleaning up data in order to match with Postgres table "transactions"
-df = df.rename(columns = {"Date":"date", "Account Running Balance": "current_bal"})
+df = df.rename(columns={
+    "Date": "date",
+    "Account Running Balance": "current_bal",
+    "Description": "description"
+})
 #saveing the changes to a new csv file, couldve done it in place but i want to keep the original and new seperated
 
-CLEANED_CSV_PATH = os.path.abspath("C:/Users/alvar/OneDrive/Desktop/Personal Finance/data/cleaned_temp.csv")
+CLEANED_CSV_PATH = os.path.abspath("C:/Users/alvar/OneDrive/Desktop/Personal_FInance_deploy/data/cleaned_temp.csv")
 df.to_csv(CLEANED_CSV_PATH, index=False)
 
-# Clear the existing data first
+# Clear the table before inserting
 cursor.execute("TRUNCATE transactions RESTART IDENTITY;")
 
-#Copying the cleaned data into the Postgres table "transactions"
-cursor.execute(f"""
-    COPY transactions(date, description, current_bal, type, amount)
-    FROM '{CLEANED_CSV_PATH}' DELIMITER ',' CSV HEADER;
-""")
+# Insert using execute_values (faster than many INSERT statements)
+insert_query = """
+    INSERT INTO transactions (date, description, current_bal, type, amount)
+    VALUES %s;
+"""
+
+# Convert DataFrame rows to list of tuples
+records = df[["date", "description", "current_bal", "type", "amount"]].values.tolist()
+
+# Execute batch insert
+psycopg2.extras.execute_values(cursor, insert_query, records)
 #Assigning categrories to the transactions
 cursor.execute("""
     UPDATE transactions
