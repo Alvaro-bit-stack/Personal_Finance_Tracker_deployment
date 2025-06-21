9461ead515eb2183ae17d18e2ad676f4f226f1e1
Alvaro-bit-stack <aizquierdo1325@gmail.com>
2025-06-21 13:42:36 -0400

    changed dashboard supabase connection

--- python_finance_dashboard.py
+++ python_finance_dashboard.py
@@ -12,11 +12,11 @@ st.set_page_config(page_title="Personal Finance Dashboard", layout="wide")
 
 # Connect to Supabase database
 conn = psycopg2.connect(
-    host=os.getenv("DB_HOST"),
-    database=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    port=os.getenv("DB_PORT")
+    host=os.getenv("host"),
+    database=os.getenv("dbname"),
+    user=os.getenv("user"),
+    password=os.getenv("password"),
+    port=os.getenv("port")
 )
 
 # --- KPI Data ---
